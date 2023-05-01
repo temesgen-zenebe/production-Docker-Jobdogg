f8f6baa2b750cfc8881def96ae5a7aa58ec4bb3d
Temesgen T Zenebe <temf2006@gmail.com>
2023-05-01 09:37:35 -0700

    editing setting

--- jobDoggApp/settings.py
+++ jobDoggApp/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_8gr_a6@+wv)r97z)8ty-%((#v_gkl4z)!%%+cui4^uvyawqpj'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 
 # Application definition
