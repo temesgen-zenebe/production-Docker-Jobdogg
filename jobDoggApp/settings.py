753c4e5a348d10625cfd1977587eda680e78efca
Temesgen T Zenebe <temf2006@gmail.com>
2023-05-11 01:23:08 -0700

    start using postgresql

--- jobDoggApp/settings.py
+++ jobDoggApp/settings.py
@@ -72,14 +72,24 @@ WSGI_APPLICATION = 'jobDoggApp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}"""
 
+DATABASES = {
+    'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'jobdogg',
+    'USER': 'postgres',
+    'PASSWORD': '1117t',
+    'HOST': 'localhost',
+    'PORT': 5432
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -115,13 +125,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 
-STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
