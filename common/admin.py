1a05b27b70e0ec49e485b18a38ebaa33fafaaf98
Temesgen T Zenebe <temf2006@gmail.com>
2023-05-17 17:22:15 -0700

    authentication of user created with emile confirmation API

--- common/admin.py
+++ common/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+admin.site.index_title = 'Home'
+admin.site.site_title = 'Jobdogg Admin'
+admin.site.site_header = 'Jobdogg Admin'
+
+
+class DjangoJokesAdmin(admin.ModelAdmin):
+    list_per_page = 25
+    list_max_show_all = 1000
+    save_as = True
--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic import UpdateView
+
+from allauth.account.views import PasswordChangeView
+
+from .forms import CustomUserChangeForm
+
+
+class CustomPasswordChangeView(LoginRequiredMixin, PasswordChangeView):
+    success_url = reverse_lazy('my-account')
+
+class MyAccountPageView(SuccessMessageMixin,LoginRequiredMixin, UpdateView):
+    model = get_user_model()
+    form_class = CustomUserChangeForm
+    template_name = 'account/my_account.html'
+    success_message = 'Update Successful'
+
+    def get_object(self):
+        return self.request.user
 
 # Create your views here.
