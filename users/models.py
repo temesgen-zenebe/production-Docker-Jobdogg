49f033e7a0cbd9022ab3278ef42a3d2f33930f94
Temesgen T Zenebe <temf2006@gmail.com>
2023-05-17 17:22:15 -0700

    authentication of user created with emile confirmation API

--- users/models.py
+++ users/models.py
@@ -1,5 +1,30 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+from django.core.files.images import get_image_dimensions
 from django.db import models
+from django.urls import reverse
 
+
+def validate_avatar(value):
+    w, h = get_image_dimensions(value)
+    if w > 200 or h > 200:
+        raise ValidationError('Avatar must be no bigger than 200x200 pixels.')
+
+# Create your models here.
 class CustomUser(AbstractUser):
-    pass
+    
+    dob = models.DateField(
+        verbose_name= "Date of Birth", null=True , blank=True
+    )
+    avatar = models.ImageField(
+        upload_to='avatars/', 
+        blank=True,
+        help_text='Image must be 200px by 200px.',
+        validators=[validate_avatar],
+    )
+    
+    def get_absolute_url(self):
+        return reverse('my-account')
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} ({self.username})'
