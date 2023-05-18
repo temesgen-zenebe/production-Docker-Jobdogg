3cf9af97f448558704216fde95f780a5ac402686
Temesgen T Zenebe <temf2006@gmail.com>
2023-05-18 04:44:31 -0700

    updating nav bar links and edited account updating

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import AboutUsView, HomePageView 
+from .views import AboutUsView, HomePageView ,ContactUsView
 
 app_name = 'pages'
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='homepage'),
     path('about-us/', AboutUsView.as_view(), name='about-us'),
+    path('contact-us/', ContactUsView.as_view(), name='contact-us'),
 ]
