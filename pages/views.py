626237ee80714f7e6093fd285e898099d174c677
Temesgen T Zenebe <temf2006@gmail.com>
2023-05-21 16:58:11 -0700

    building user dashboard interface per user group

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import TemplateView
 from django.contrib import messages
 from django.shortcuts import render,redirect
-
+from users.templatetags.user_tags import is_admin, is_employee, is_employer
 
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
@@ -22,8 +22,12 @@ class ContactUsView(TemplateView):
 
 
 def dashboard(request):
-    if request.user.is_authenticated:
-        return render(request, 'pages/dashboard.html')
+    if request.user.is_authenticated and is_admin(request.user):
+        return render(request, 'pages/admin_dashboard.html')
+    elif request.user.is_authenticated and is_employee(request.user):
+        return render(request, 'pages/employee_dashboard.html')
+    elif request.user.is_authenticated and is_employer(request.user):
+        return render(request, 'pages/employer_dashboard.html')
     else:
         return redirect('login')
 
