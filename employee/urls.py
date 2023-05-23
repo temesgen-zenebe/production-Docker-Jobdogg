6c13d24adf8873d3d4c999dc2843f77a6c3eee16
Temesgen T Zenebe <temf2006@gmail.com>
2023-05-22 20:51:43 -0700

    adding list_view templates employee accepting policy

--- employee/urls.py
+++ employee/urls.py
@@ -6,14 +6,21 @@ from .views import (
     PoliciesCreateView,
     PoliciesUpdateView,
     PoliciesDeleteView,
+    PolicyListView,
+    AcceptPoliciesView,
 )
-app_name = 'employee'
+app_name = 'employee' 
 
 urlpatterns = [
-    path('policies/', PoliciesListView.as_view(), name='policies_list'),
+    path('policies/list/', PolicyListView.as_view(), name='policies_list'),
+    path('policies/accept/', AcceptPoliciesView.as_view(), name='accept_policies'),
     path('policies/<slug:slug>/', PoliciesDetailView.as_view(), name='policies_detail'),
     path('policies/accept/<slug:slug>/', PoliciesAcceptView.as_view(), name='policies_accept'),
     path('policies/create/', PoliciesCreateView.as_view(), name='policies_create'),
     path('policies/update/<slug:slug>/', PoliciesUpdateView.as_view(), name='policies_update'),
     path('policies/delete/<slug:slug>/', PoliciesDeleteView.as_view(), name='policies_delete'),
 ]
+
+
+
+
