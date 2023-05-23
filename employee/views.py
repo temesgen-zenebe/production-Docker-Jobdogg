73e431c58e4aa00a87669c9b365d1bce00fad5aa
Temesgen T Zenebe <temf2006@gmail.com>
2023-05-22 20:51:43 -0700

    adding list_view templates employee accepting policy

--- employee/views.py
+++ employee/views.py
@@ -1,9 +1,73 @@
-from django.shortcuts import redirect
+
 from django.views.generic import ListView, DetailView, FormView,CreateView,UpdateView,DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from .models import Policies, UserAcceptedPolicies
+from django.contrib import messages
 from django import forms
+from django.shortcuts import render, redirect
+from django.views import View
+
+class PolicyListView(LoginRequiredMixin, View):
+    template_name = 'employee/policy_list.html'
+    context_object_name = 'policies'
+
+    def get(self, request):
+        policies = Policies.objects.all()
+        accepted_policies = UserAcceptedPolicies.objects.filter(user=request.user)
+        accepted_policies_ids = [policy.policies_id for policy in accepted_policies]
+        total_policies_count = policies.count()
+        accepted_policies_count = len(accepted_policies_ids)
+        all_policies_accepted = accepted_policies_count == total_policies_count
+
+        context = {
+             self.context_object_name: policies,
+            'accepted_policies_ids': accepted_policies_ids,
+            'all_policies_accepted': all_policies_accepted,
+        }
+       
+        return render(request, self.template_name, context)
+        
+
+    def post(self, request):
+        
+        policies = Policies.objects.all()
+        accepted_policies = []
+
+        for policy in policies:
+            accepted = request.POST.get(f'policy_{policy.id}')
+            if accepted == 'on':
+                accepted_policies.append(UserAcceptedPolicies(user=request.user, policies=policy, accepted=True))
+
+        UserAcceptedPolicies.objects.bulk_create(accepted_policies)
+
+        # Update is_accepted context states to True
+        request.session['is_accepted'] = True
+
+        messages.success(request, 'Policies accepted successfully.')
+        return redirect('employee:policies_list')
+        
+
+class AcceptPoliciesView(LoginRequiredMixin, View):
+    def post(self, request):
+        user = request.user
+        policies = UserAcceptedPolicies.objects.filter(user=user, accepted=False)
+
+        if policies:
+            for policy in policies:
+                policy.accepted = True
+                policy.save()
+
+            # Update BasicInfo model
+            user.profile.is_accepted = True
+            user.profile.save()
+
+            # Redirect to success page or home page
+            return redirect('employee:policies_list')
+
+        # Redirect to some error page if no policies found
+        return redirect('employee:policies_list')
+
 
 
 class PoliciesAcceptForm(forms.Form):
---
+++ users/templatetags/policy_tags.py
@@ -0,0 +1,12 @@
+from django import template
+from employee.models import UserAcceptedPolicies
+
+register = template.Library()
+
+@register.filter
+def check_accepted_policy(user_accepted_policies, policy):
+    try:
+        user_accepted_policy = user_accepted_policies.get(policies=policy)
+        return user_accepted_policy.accepted
+    except UserAcceptedPolicies.DoesNotExist:
+        return False
