94e179b9fb1721599c16db0eafcb29242d4975c0
Temesgen T Zenebe <temf2006@gmail.com>
2023-06-12 03:11:51 -0700

    urls updating

--- employee/views.py
+++ employee/views.py
@@ -760,7 +760,7 @@ class ExperienceDeleteView(LoginRequiredMixin, DeleteView):
     def get_queryset(self):
         return super().get_queryset().filter(user=self.request.user)
     
-# NoExperienceView   
+# No ExperienceView   
 class NoExperienceView(LoginRequiredMixin, View):
     def post(self, request):
         profile = get_object_or_404(Profile, user=request.user)
