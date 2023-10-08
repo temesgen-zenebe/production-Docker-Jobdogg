219879708e833a7e43d1fcf72040ad61b3c0efc8
Temesgen T Zenebe <temf2006@gmail.com>
2023-10-08 00:50:38 -0700

    updating  setting.py and staticfiles

--- jobDoggApp/settings.py
+++ jobDoggApp/settings.py
@@ -179,9 +179,10 @@ USE_I18N = True
 USE_TZ = True
 # # Local Static File Settings
 
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 MEDIA_URL = '/media/'
